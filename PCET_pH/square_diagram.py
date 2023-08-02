d4a056d578fb3472daefdd534c6c4574f799cc01
kcui7 <39925154+kcui7@users.noreply.github.com>
2023-08-01 23:55:13 -0400

    Update square_diagram.py

--- PCET_pH/square_diagram.py
+++ PCET_pH/square_diagram.py
@@ -2,8 +2,8 @@ import numpy as np
 
 class GeneralizedSquareDiagram(object):
     """
-    This class stores all thermodynamic paramaters which will be used to calculate the mole fractions at given pH. 
-    This class also enables the use of potential-dependent paramters by
+    This class stores all thermodynamic parameters which will be used to calculate the mole fractions at given pH. 
+    This class also enables the use of potential-dependent parameters by
     using callable functions as the K or E_ET1 parameters. 
 
     Input description and data type:
